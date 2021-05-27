0a7613ed5850776b07b8fdd4dfb3cddc688f88c4
Sarbjotm <sarbjotmann98@gmail.com>
2021-05-27 00:18:24 -0700

    Lowest Degree and Highest Degree elements added

--- Assignments/A1/A1.py
+++ Assignments/A1/A1.py
@@ -37,12 +37,42 @@ def network_density(N, L):
     return numerator / denominator
 
 
+def highest_degree(G):
+    max_value = 0
+    nodes_with_max_value = []
+    for node in G.nodes():
+        if G.degree(node) > max_value:
+            max_value = G.degree(node)
+
+    for node in G.nodes():
+        if G.degree(node) == max_value:
+            nodes_with_max_value.append(node)
+
+    return nodes_with_max_value
+
+
+def lowest_degree(G):
+    min_value = 1
+    nodes_with_min_value = []
+    for node in G.nodes():
+        if G.degree(node) < min_value:
+            min_value = G.degree(node)
+
+    for node in G.nodes():
+        if G.degree(node) == min_value:
+            nodes_with_min_value.append(node)
+
+    return nodes_with_min_value
+
+
 if __name__ == '__main__':
     graph = main()
     edges = number_of_edges(graph)
     nodes = number_of_nodes(graph)
     max_edges_possible = number_of_possible_edges(nodes)
     density = network_density(nodes, edges)
+    max_degree = highest_degree(graph)
+    min_degree = lowest_degree(graph)
     print(f"The number of edges in our graph is {edges}, and the number of nodes in our graph is {nodes}. \n"
           f"The max amount of edges possible in a undirected graph with {nodes} nodes is {max_edges_possible} \n"
           f"The density of our network is {density}.")
