2c4cf668314d4c7f969b4babb03944d51a219e2c
Sarbjotm <sarbjotmann98@gmail.com>
2021-06-15 00:17:52 -0700

    A3 extended experiment

--- Assignments/A3/A3.py
+++ Assignments/A3/A3.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import statistics
+import random
 
 from Assignments.A1.A1 import number_of_nodes, number_of_edges, highest_degree, lowest_degree, average_degree, \
     histogram_degrees
+
+from Assignments.A2.A2 import shortest_path
 
 
 def main():
@@ -38,6 +41,56 @@ def main():
     print('Mean betweenness:', statistics.mean(betweenness_sequence))
     mean_closeness = closeness_mean(graph)
     print(f"Mean closeness is {mean_closeness}")
+
+    # Create two nodes list
+    list_of_nodes = node_list_create(graph)
+
+    list_of_nodes_random = node_list_create(graph)
+
+    list_of_nodes = node_list_sort(list_of_nodes, graph, nodes)
+    list_of_nodes = list_of_nodes[0:8]
+    random.shuffle(list_of_nodes_random)
+
+    average_path_random = remove_nodes(list_of_nodes_random)
+
+    print(f"Removing two orders in random degree makes the path {average_path_random}")
+    print(f"Nodes in decreasing order of degree are {list_of_nodes}")
+    connected_print(graph, list_of_nodes)
+
+
+def connected_print(G, nodes_list):
+    for i in range(0, len(nodes_list)):
+        for j in range(0, len(nodes_list)):
+            if i == j:
+                pass
+            elif G.has_edge(nodes_list[i], nodes_list[j]):
+                print(f"There exists an edge between {nodes_list[i]} {nodes_list[j]}")
+
+        print("\n \n")
+
+
+def remove_nodes(nodes_list):
+    graph_r = nx.read_adjlist('../Datasets/vitevitch.adjlist')
+    for i in range(0, 2):
+        graph_r.remove_node(nodes_list[i])
+    return shortest_path(graph_r)
+
+
+def node_list_sort(list_of_nodes, graph, nodes):
+    for i in range(0, nodes):
+        for j in range(i + 1, nodes):
+            if graph.degree(list_of_nodes[j]) > graph.degree(list_of_nodes[i]):
+                tmp = list_of_nodes[j]
+                list_of_nodes[j] = list_of_nodes[i]
+                list_of_nodes[i] = tmp
+    return list_of_nodes
+
+
+def node_list_create(G):
+    node_list = []
+    for node in G.nodes():
+        node_list.append(node)
+    return node_list
 
 
 def closeness_mean(G):
